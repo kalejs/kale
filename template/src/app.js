baed5337f32e25b18b5cef91e961fcfc3d8495c2
Matt Venables <mattvenables@gmail.com>
2015-07-26 19:40:08 -0400

    Add CORS

--- template/src/app.js
+++ template/src/app.js
@@ -3,6 +3,7 @@
 var app = require('koa')();
 var body = require('koa-json-body');
 var config = require('./config');
+var cors = require('kcors');
 var helmet = require('koa-helmet');
 var json = require('koa-json');
 var logger = require('koa-morgan').middleware;
@@ -11,6 +12,7 @@ var router = require('./routes');
 
 app.use(logger(config.logging.format));
 app.use(helmet());
+app.use(cors());
 app.use(body());
 app.use(json(config.json));
 app.use(middleware.config);
