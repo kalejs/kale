c6aa0725af0e86206dee743d768fd848f19abed1
Matt Venables <mattvenables@gmail.com>
2016-11-23 18:24:34 -0500

    Update scaffold generator

--- generators/scaffold.js
+++ generators/scaffold.js
@@ -1,22 +1,13 @@
 'use strict';
 
-var _ = require('lodash');
-_.str = require('underscore.string');
-_.str.inflection = require('inflection');
-
 module.exports = function(name, options) {
   var modelGenerator = require('./model');
   var controllerGenerator = require('./controller');
   var viewGenerator = require('./view');
 
-  var underscored = _.str.underscored(name);
-  var singular = _.str.inflection.singularize(underscored);
-  var camelized = _.str.camelize(singular);
-  var controllerName = _.str.inflection.pluralize(camelized);
-
-  modelGenerator(singular, options);
-  controllerGenerator(controllerName, options);
-  viewGenerator(controllerName, options);
+  modelGenerator(name, options);
+  controllerGenerator(name, options);
+  viewGenerator(name, options);
 
   console.log(`Scaffolding generated.`);
 };
